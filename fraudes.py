642f4ba0d4cdeefcc5313b122fbba3c83040913b
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-03-07 08:45:38 -0300

    feat: adde new file

---
+++ fraudes.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Carregar o CSV, removendo espaços extras dos nomes das colunas
+fraude = pd.read_csv("./Table_fraudes.csv", sep=";", encoding="utf-8")
+fraude.columns = fraude.columns.str.strip()  # Remove espaços extras nos nomes das colunas
+
+# Converter TITULO_ELEITORAL para string
+fraude["TITULO_ELEITORAL"] = fraude["TITULO_ELEITORAL"].astype(str)
+
+# Adicionar zero à esquerda se o número tiver menos de 12 dígitos
+fraude["TITULO_ELEITORAL"] = fraude["TITULO_ELEITORAL"].apply(lambda x: x.zfill(12))
+
+# Exibir o DataFrame corrigido
+fraude.to_csv("./Table_fraudes.csv", sep=";", encoding="utf-8")
